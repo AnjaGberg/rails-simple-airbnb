ad042088dfbd680771447cb68ccbf96881d43320
Anja Greifenberg <anja.gberg@gmx.de>
2022-02-16 16:51:58 +0100

    basic functions and layout done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+4.times do |flat|
+  Flat.create!(
+    name: Faker::Hipster.sentence(word_count: 3),
+    address: Faker::Address.full_address,
+    description: Faker::Hipster.paragraph(sentence_count: 2),
+    price_per_night: rand(39..118),
+    number_of_guests: rand(2..6)
+  )
+  puts "Flat #{flat} done"
+end
+
+puts 'All flats created'
